aa3d81652e9331e04e00c364f938375edf643a8e
antoniosenra <antoniopsenra@gmail.com>
2017-01-07 14:31:40 -0600

    Re-ordering the HTML script introductions.
    The internet says that by re-ordering the HTML script introductions
    (i.e., making the jQuery script introduction come before the JS script
    introduction), we can avoid a situation in which the jQuery fails
    because the jQuery function happens before the jQuery script has loaded.

--- N-A_Javascript.js
+++ N-A_Javascript.js
@@ -1,5 +1,1 @@
 console.log("testing console log");
-
-$.ajax({cache: false, url: "https://haveibeenpwned.com/api/v2/breachedaccount/antoniopsenra@yahoo.com", success: function(data) {
-        console.log(data);
-}});
