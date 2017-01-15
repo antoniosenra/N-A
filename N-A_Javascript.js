4f235b3aced188440e16b2c4939f6dd5bd0bee01
antoniosenra <antoniopsenra@gmail.com>
2017-01-14 20:56:48 -0600

    Added logs for testing + deleted variable
    Creating the variable "i" was unnecessary for the "for loop." I deleted
    it. I also added some console.logs to test some of the code.

--- N-A_Javascript.js
+++ N-A_Javascript.js
@@ -35,6 +35,8 @@ function loadDoc() {
 
 //This section describes the error message that's produced if the API returns no results. We can come up with something better than this.
 
+      console.log("The ready state test produced a 404.")
+
       document.getElementById("breachInfo").innerHTML = "<h4><p>404 - No data returned for  <span class=\"attention\">\"" + email + ".\"</span> This is a good thing! It appears you have no known associated breaches with this account.</p></h4>";
     }
   };
@@ -52,14 +54,20 @@ function writeData(response) {
 
 //I added this console log so we can get an understanding of what the API is
 
+  console.log("This is the result of console logging the array:");
   console.log(arr);
+  console.log("This is the length of the array:" + arr.length);
 
 //There is a variable just called "i" that isn't defined? What is the purpose of this? We should figure out how this works.
 
 //It appears to be a variable created for counting purposes. It appears necessary to run the for/while loop that exists below.
 
-  var i;
   var email = document.getElementById("checkEmail").value;
+
+//What does the line directly below mean? What value does the "span class" attribute add?
+
+//With each for loop, the "out" variaable is lengthened.
+
   var out = "<h4><span class=\"attention\">Unfortunately, it looks like \"" + email + "\" has been breached. </span>Please see the details below:</h4>" + "<div class=\"breachEntries\">";
   for(i = 0; i < arr.length; i++) {
     out += "<div class=\"breachEntry\" id=\"breachEntry" + i + "\"><div class=\"breachDomain\"><strong>Domain of Breach:&nbsp;</strong>" + arr[i].Name +
