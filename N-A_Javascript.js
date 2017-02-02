6f43e210839986222542a8ab79973a3f44409ee1
omar <OmarAlami24@gmail.com>
2017-02-01 23:38:36 -0500

    Quick emulation of mockup

--- N-A_Javascript.js
+++
@@ -1,5 +0,0 @@
-console.log("testing console log");
-
-$.ajax({cache: false, url: "https://haveibeenpwned.com/api/v2/breachedaccount/antoniopsenra@yahoo.com", success: function(data) {
-        console.log(data);
-}});
