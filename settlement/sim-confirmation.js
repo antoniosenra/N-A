7757a1783e0ea2f699b889a07fbc5449196cb49b
antoniosenra <antoniopsenra@gmail.com>
2017-03-12 20:42:45 -0500

    Added back buttons and improved confirmation pg
    I converted "Zeroday" at the top of every page into a link that sends
    them back to index.html.
    
    I also added a simulation "download" button to the
    simulation-confirmaation page.

--- settlement/sim-confirmation.js
+++ settlement/sim-confirmation.js
@@ -1,1 +1,8 @@
 console.log("testing console log on sim-confirmation.js");
+
+function revealDownloadInfo() {
+  document.getElementById("DownloadInfo").innerHTML += "<p>If this were not a simulation, clicking \
+  \"Download Completed Settlement Form\" would have allowed you to download a PDF of your \
+  completed Settlement Form. All that would be left for you to do is print that form and mail it \
+  to the company managing the settlement you're involved in.</p>";
+}
